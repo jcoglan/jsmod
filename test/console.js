5e21746868a8c11cf35d7170f0a02396d031c59c
James Coglan <jcoglan@gmail.com>
2012-07-18 23:03:24 +0100

    Make test work on WSH.

--- test/console.js
+++ test/console.js
@@ -1,4 +1,21 @@
-if (typeof print !== 'function') print = console.log;
+if (this.ActiveXObject)
+  load = function(path) {
+    var fso = new ActiveXObject('Scripting.FileSystemObject'), file, runner;
+    try {
+      file   = fso.OpenTextFile(path);
+      runner = function() { eval(file.ReadAll()) };
+      runner();
+    } finally {
+      try { if (file) file.Close() } catch (e) {}
+    }
+  };
+
+if (typeof print !== 'function') {
+  if (typeof console === 'object')
+    print = function(s) { console.log(s) };
+  else if (typeof WScript === 'object')
+    print = function(s) { WScript.Echo(s) };
+}
 
 if (typeof require === 'function') {
   var poly = require('../lib/jsmod/polynomial').polynomial;
